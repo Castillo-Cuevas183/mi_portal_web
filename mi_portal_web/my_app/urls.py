e33554b1c8458ad497b5405e5d75604654834036
Castillo-Cuevas183 <castillo.cri@gmail.com>
2024-09-27 10:43:42 -0300

    se suben páginas

--- mi_portal_web/my_app/urls.py
+++ mi_portal_web/my_app/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name='home'), # El que tiene solo comillas es la página principal
+    path('home', views.home, name='home'), # El que tiene solo comillas es la página principal
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),  # Add this line
     path('blog/', views.blog, name='blog'),  # Add this line
     path('services/', views.services, name='services'),  # Add this line
+    path('our_teams/', views.our_teams, name='our_teams'),  # Add this line
+    path('portafolio/', views.portafolio, name='portafolio'),  # Add this line
+    path('testimonios/', views.testimonios, name='testimonios'),  # Add this line
 ]
 
 
--- mi_portal_web/my_app/views.py
+++ mi_portal_web/my_app/views.py
@@ -21,4 +21,27 @@ def services (request):
     # return HttpResponse("Estás en la sección 'Services'.")
     return render (request, 'myapp/services.html')
 
+def our_teams (request):
+    return render (request, 'myapp/our_teams.html')
 
+# def portafolio (request):
+#     return render (request, 'myapp/portafolio.html')
+
+def portafolio (request):
+    projects =[
+        {'name': 'Pagina de Restaurant', 'description': 'Proyecto para cadena de restaurantes mexicanos', 'image': '' },
+        {'name': 'Pagina de Inmobiliarias', 'description': 'Proyecto para venta de casas', 'image': '' },
+        {'title': 'Pagina de Escuelas', 'description': 'Descripción del proyecto 3'},
+        #... more projects here
+    ]
+    return render(request, 'myapp/portafolio.html', {'projects': projects})
+
+
+def testimonios (request):
+
+    testimonios_data =[
+        {'Nombre': "Roberto", 'Puesto': "CEO", 'testimonio': 'Excelente servicio' },
+        {'Nombre': "Roberto", 'Puesto': "CEO", 'testimonio': 'Excelente servicio' },
+        #... more projects here
+    ]
+    return render(request, 'myapp/testimonios.html', {'testimonios': testimonios_data})
